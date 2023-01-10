49b88dec9612a6df0f47bc70775c11b394ba096e
Timur Mukhametzyanov <bratov.tima@mail.ru>
2023-01-10 19:50:21 +0400

    add logic for sum items in basket and logic for delete all items or unic item

--- scripts/basket.js
+++ scripts/basket.js
@@ -1,7 +1,7 @@
-const basket = document.querySelector(".card-basket");
-const basketBox1 = document.querySelector('.circle');
+const basketCard = document.querySelector(".card-basket");
+const circleCountItemsInBasket = document.querySelector('.circle');
 
-let countitem = 0;
+let countItemInBasket = 0;
 // обращаемся ко всему окну
 window.addEventListener("click", function(event) {
     
@@ -20,8 +20,19 @@ window.addEventListener("click", function(event) {
             priceNumber: +card.querySelector('.newPrice').innerHTML.slice(2, -3),
         };
         
-        // карточка товара, то есть так как она будет вставляться в блок корзины
-        const cardItem = `
+        // проверить, есть ли товар в корзине, для суммирования кол-ва, а не добавления новых карточек
+        // если товара нет, то добавляем карточку новую
+        const itemInCard = basketCard.querySelector(`[data-id='${product.id}']`);
+        if(itemInCard) {
+            const newCounter = itemInCard.querySelector(`[data-counter]`);
+            const newPrice = itemInCard.querySelector(".newPrice");
+            newCounter.innerHTML = parseInt(newCounter.innerHTML) + +(product.counter) + ` pcs.`;
+            newPrice.innerHTML = `$ ` + newCounter.innerHTML.slice(0, -4) * product.priceNumber;
+            console.log(newCounter.innerHTML.slice(0, -5));
+        } else {
+
+            // карточка товара, то есть так как она будет вставляться в блок корзины
+            const cardItem = `
             <div class="item-product" data-id="${product.id}">
                 <img src="${product.img}" alt="headphones">
                 <div class="price-and-box">
@@ -35,21 +46,48 @@ window.addEventListener("click", function(event) {
                             </div>
                         </div>
                         <div class="buttons">
-                            <div class="button-count" data-counter>${product.counter + " шт."}</div>
+                            <div class="button-count" data-counter>${product.counter + " pcs."}</div>
                         </div>
                     </div>
+                    <div class="delete-item">Delete this item</div>
                 </div>
             </div>
         `;
-        
+
+        // вставка нашего товара в наш блок корзины
+        basketCard.insertAdjacentHTML('beforeend', cardItem);
+        }
+       
         // кол-во товаров в корзине
-        countitem += +product.counter;
+        countItemInBasket += +product.counter;
+        circleCountItemsInBasket.innerHTML = `${countItemInBasket}`;
 
-         // вставка нашего товара в наш блок корзины
-        basket.insertAdjacentHTML('beforeend', cardItem);
+        // сброс счетчика data-counter после добавления товара
+        card.querySelector(`[data-counter]`).innerHTML = 1;
 
-        basketBox1.innerHTML = `${countitem}`;
     }
-        
+
+    // удаление всех товаров из корзины 
+    const cardBasket = document.querySelector('.card-basket');
+    if(event.target.closest(".delete-all") && countItemInBasket > 0) {
+
+        // смотрим на ласт элемент нашего родителя
+        let childNow = cardBasket.lastChild;
+
+        // пока он есть, то есть пока условие тру, мы удаляем последний элемент и находим новый последний элемент
+        // дальше все по кругу
+        while (childNow) {
+            cardBasket.removeChild(childNow);
+            childNow = cardBasket.lastChild;
+        }
+
+        // обнуление счетчика товаров в корзине
+        circleCountItemsInBasket.innerHTML = 0;
+        countItemInBasket = 0;
+    }
+
+    if(event.target.closest(".delete-item") && countItemInBasket > 0) {
+        cardBasket.removeChild(document.querySelector(`[data-id]`));
+    }
 });
 
